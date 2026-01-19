f0b1e8ce4fb7264bb70833b8bb61d8d2f336a4db
Anish Pentyala <anish.n.pentyala@gmail.com>
2026-01-19 20:14:52 +0000

    Student Access Page Redesign

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Problem from '@/components/home/Problem';
 import Programs from '@/components/home/Programs';
 import CommunitySurvey from '@/components/home/CommunitySurvey';
 import Impact from '@/components/home/Impact';
+import GetInvolved from '@/components/home/GetInvolved';
 import Partnerships from '@/components/home/Partnerships';
 import About from '@/components/home/About';
 import Contact from '@/components/home/Contact';
@@ -19,6 +20,7 @@ export default function Home() {
       <Programs />
       <CommunitySurvey />
       <Impact />
+      <GetInvolved />
       <Partnerships />
       <Contact />
       <Footer />
